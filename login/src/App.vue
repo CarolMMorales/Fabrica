3c7fa77bc42b72b8e95d3f0be7c8734b21a7ac0f
CarolMMorales <carolmoralfs@gmail.com>
2024-01-25 21:19:34 -0500

    first commit

---
+++ login/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import router from './router/index';
+</script>
+<template>
+  <nav class="navbar mb-5 mt-2">
+    <div class="container ">
+      <router-link class="navbar-brand" to="/">
+        <img alt="UE logo" width="380" height="70" class="d-inline-block img-responsive" src="@/assets/Logo_UE_CCB.png" />
+      </router-link>
+    <!--
+    <div>
+      
+      <router-link class="btn btn-link m-4 fs-4" to="/">Iniciar Sesion</router-link>
+      
+      <router-link class="btn btn-link fs-5" to="/register">Registrarse</router-link>
+
+    </div>
+    -->
+  </div>
+</nav>
+    <!--
+    <img alt="UE logo" class="img-fluid" src="@/assets/Logo_UE_CCB.png" />
+    -->
+   <div class="">
+    <RouterView />
+   </div>
+  
+</template>
---
+++ login/src/views/RegisterView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Carousel from "../components/Carousel.vue";
+</script>
+
+<template>
+  <div class="register">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-ms-6">
+        <carousel></carousel>
+      </div>
+    <div class="col-md-6 col-ms-6 border-2 ps-5 ">
+      <form action="" >
+        <h3 class="text-center ps-3" style="color: #dd0034
+;">Registrate</h3>
+        <div class="mb-3 ps-3">
+    <label for="exampleInputEmail1" class="form-label text-start ps-3">Ingrese su correo institucional</label>
+    <input type="email" class="form-control ps-3" id="exampleInputEmail1" aria-describedby="emailHelp">
+  
+  </div>
+  <div class="mb-3 ps-3">
+    <label for="exampleInputPassword1" class="form-label ps-3">Contraseña</label>
+    <input type="password" class="form-control ps-3" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-inputps-3" id="exampleCheck1">
+    <label class="ps-2 form-check-label" for="exampleCheck1">Acepta terminos y condiciones</label>
+  </div>
+  <div>
+      
+      <router-link class="btn btn-link fs-6" to="/">Iniciar Sesion</router-link>
+     <!-- 
+      <router-link class="btn btn-link fs-6" to="/register">Registrarse</router-link>
+-->
+    </div>
+  <div class="text-center">
+  <button type="submit" class="btn btn-outline-danger p-2 align-content-center">Crear Cuenta</button>
+</div>  
+      </form>
+    
+    </div>
+  </div>
+</div>
+</template>
+
+
